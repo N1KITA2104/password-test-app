fa2a5c16fe2e50536bd81c25ce193fed4047e05c
Nikita Apatiev <herobrineset21@gmail.com>
2024-09-05 23:32:35 +0300

    Initialize Angular project and set up basic structure:
    - Create new Angular project with default configuration
    - Add initial project files and directory structure
    - Install Roboto font in styles.scss
    - Create PasswordComponent
    - Commit initial setup with no additional functionality implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PasswordComponent } from './components/password/password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'password',
+        component: PasswordComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'password',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'password'
+    }
+];
